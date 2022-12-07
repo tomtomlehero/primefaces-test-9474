a2d09a9286bc035880edc53b3e89d0419a129b36
Mathieu Lanfranchi <mathieu.lanfranchi@acensi.fr>
2022-12-07 17:59:49 +0100

    Test project for Issue 9474

--- src/main/java/org/primefaces/test/TestView.java
+++ src/main/java/org/primefaces/test/TestView.java
@@ -3,12 +3,17 @@ package org.primefaces.test;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
+
 import javax.annotation.PostConstruct;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 
 import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Data
 @Named
 @ViewScoped
@@ -22,6 +27,24 @@ public class TestView implements Serializable {
     @PostConstruct  
     public void init() {
         string = "Welcome to PrimeFaces!!!";
+    }
+
+
+
+    public boolean isRendered() {
+        return false;
+    }
+
+    public List<String> getCountries() {
+        return Arrays.asList("Argentina", "Australia", "Brazil", "Canada");
+    }
+
+    public List<String> getColumns() {
+
+        if (!isRendered()) {
+            log.error("You should never read this");
+        }
+        return Arrays.asList("Countries");
     }
 
 }
